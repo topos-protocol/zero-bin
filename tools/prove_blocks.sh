0ffa5ae0582f7124a7b09c24e360393f43d0c748
John Hilliard <praetoriansentry@gmail.com>
2023-12-12 09:10:49 -0500

    fix: upping the prover circuit sizes and making filenames sortable

--- tools/prove_blocks.sh
+++ tools/prove_blocks.sh
@@ -28,16 +28,20 @@ for ((i=$1; i<=$2; i++))
 do
     echo "Proving block ${i}..."
 
-    OUT_PROOF_PATH="${PROOF_OUTPUT_DIR}/b${i}.zkproof"
-    OUT_LOG_PATH="${PROOF_OUTPUT_DIR}/b${i}.log"
+    out_idx=$(printf "%05d" $i)
 
-    if [ $i -gt 1 ]; then
+    OUT_PROOF_PATH="${PROOF_OUTPUT_DIR}/b${out_idx}.zkproof"
+    OUT_LOG_PATH="${PROOF_OUTPUT_DIR}/b${out_idx}.log.bz2"
+
+    if [[ $i -gt 1 ]]; then
         prev_proof_num=$((i-1))
-        PREV_PROOF_EXTRA_ARG="-f ${PROOF_OUTPUT_DIR}/b${prev_proof_num}.zkproof"
+	prev_proof_num_idx=$(printf "%05d" $prev_proof_num)
+        PREV_PROOF_EXTRA_ARG="-f ${PROOF_OUTPUT_DIR}/b${prev_proof_num_idx}.zkproof"
     fi
 
     # echo "cheese"
     # echo "cargo r --release --bin leader -- --runtime in-memory jerigon --rpc-url "$3" --block-number $i --proof-output-path $OUT_PROOF_PATH $PREV_PROOF_EXTRA_ARG > $OUT_LOG_PATH 2>&1"
+<<<<<<< HEAD
     cargo r --release --bin leader -- --runtime in-memory jerigon --rpc-url "$3" --block-number $i --proof-output-path $OUT_PROOF_PATH $PREV_PROOF_EXTRA_ARG > $OUT_LOG_PATH 2>&1
 
     retVal=$?
@@ -48,8 +52,24 @@ do
     else
         # Remove the log on success if we don't want to keep it.
         if [ $ALWAYS_WRITE_LOGS -ne 0 ]; then
+=======
+    cargo r --release --bin leader -- \
+	  --runtime in-memory jerigon \
+	  --rpc-url "$3" \
+	  --block-number $i \
+	  --proof-output-path $OUT_PROOF_PATH \
+	  $PREV_PROOF_EXTRA_ARG 2>&1 | bzip2 > $OUT_LOG_PATH
+    # touch $OUT_LOG_PATH
+    # touch $OUT_PROOF_PATH
+
+    if [[ -e $OUT_PROOF_PATH ]]; then
+        if [[ $ALWAYS_WRITE_LOGS -ne 0 ]]; then
+>>>>>>> 10743b9 (fix: upping the prover circuit sizes and making filenames sortable)
             rm $OUT_LOG_PATH
         fi
+    else
+	echo "Block ${i} errored. See ${OUT_LOG_PATH} for more details."
+	exit 1
     fi
 done
 
