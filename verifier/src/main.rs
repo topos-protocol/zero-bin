44cc531f418d7452334b32acb93d9249f3ae95c8
Robin Salen <salenrobin@gmail.com>
2024-01-19 08:31:54 -0500

    Load verifier state from disk if existing

--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -2,11 +2,17 @@ use std::fs::File;
 
 use anyhow::Result;
 use clap::Parser;
-use plonky_block_proof_gen::{prover_state::ProverStateBuilder, types::PlonkyProofIntern};
+use common::prover_state::{set_prover_state_from_config, P_STATE};
+use plonky_block_proof_gen::{prover_state::ProverState, types::PlonkyProofIntern};
 use serde_json::Deserializer;
+use tracing::warn;
 
 mod cli;
 mod init;
+
+fn p_state() -> &'static ProverState {
+    P_STATE.get().expect("Prover state is not initialized")
+}
 
 fn main() -> Result<()> {
     init::tracing();
@@ -16,9 +22,11 @@ fn main() -> Result<()> {
     let des = &mut Deserializer::from_reader(&file);
     let input: PlonkyProofIntern = serde_path_to_error::deserialize(des)?;
 
-    let prover = ProverStateBuilder::default().build();
+    if set_prover_state_from_config(args.prover_state_config.into()).is_err() {
+        warn!("prover state already set. check the program logic to ensure it is only set once");
+    }
 
-    prover.state.verify_block(&input)?;
+    p_state().state.verify_block(&input)?;
 
     Ok(())
 }
